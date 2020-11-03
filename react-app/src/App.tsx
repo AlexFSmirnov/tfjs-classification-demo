1fbd98279b50021887c8c1cbac6e424a1abf5f1b
Alexander Smirnov <alex.f.smirnov@gmail.com>
2020-11-03 22:44:12 +0100

    added model loading and prediction logic

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
+import { loadLayersModel, tensor, LayersModel, Tensor } from '@tensorflow/tfjs';
 import { CharacterCanvas } from './components';
+import { IMAGE_SIZE, CLASSES } from './constants';
 
-function App() {
-    return (
-        <CharacterCanvas />
-    );
+class App extends React.Component {
+    private model: LayersModel | null = null;
+
+    componentDidMount() {
+        this.loadModel();
+    }
+
+    loadModel = async () => {
+        this.model = await loadLayersModel(`${process.env.PUBLIC_URL}/model/model.json`);
+        console.log('Model loaded');
+    };
+
+    predict = async (pixels: number[][][]) => {
+        if (!this.model) {
+            return;
+        }
+
+        const inputTensor = tensor([pixels], [1, IMAGE_SIZE, IMAGE_SIZE, 3]);
+
+        const result = this.model.predict(inputTensor) as Tensor;
+        const predictionArray = Array.from(result.dataSync());
+
+        const maxProb = Math.max(...predictionArray);
+        const idx = predictionArray.indexOf(maxProb);
+        const hexVal = CLASSES[idx].split('_')[CLASSES[idx].split('_').length - 1];
+        const val = String.fromCharCode(parseInt(hexVal, 16));
+        console.log(`${hexVal}: ${val}   (${maxProb})`);
+    };
+
+    handleCanvasChange = (pixels: number[][][] | null) => {
+        if (!pixels) {
+            return;
+        }
+
+        this.predict(pixels);
+    };
+
+    render() {
+        return (
+            <CharacterCanvas onChange={this.handleCanvasChange} />
+        );
+    }
 }
 
 export default App;
